ac9d73284cc24f492842daee74f3103c7e58839b
To Ngoc Hieu <hieudeptrai09@gmail.com>
2026-08-13 15:45:04 +0700

    Revert "thêm bộ source cần chuyển"
    This reverts commit 2871a098fe3c246812b00b0319f381ef77ea3523.

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: true,
-  singleQuote: false,
-  trailingComma: "all",
-  plugins: [
-    "prettier-plugin-tailwindcss",
-    "@ianvs/prettier-plugin-sort-imports",
-  ],
-};
